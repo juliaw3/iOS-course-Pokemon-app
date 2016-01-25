619101db5d5a0935d81b58b7830a22626df11671
Julia Waggoner <jjwaggoner27@gmail.com>
2016-01-25 13:23:08 -0600

    parse the json

---
+++ Pokemon/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Pokemon
+//
+//  Created by Julia Waggoner on 1/25/16.
+//  Copyright © 2016 Julia Waggoner. All rights reserved.
+//
+
+import Foundation
+
+let URL_BASE = "http://pokeapi.co"
+let URL_POKEMON = "/api/v1/pokemon/"
+
+typealias DownloadComplete = () -> ()
