2417e5e6bdcccf6317e125a07e25e69af9a1aa4c
Julia Waggoner <jjwaggoner27@gmail.com>
2016-01-24 23:41:24 -0600

    parse the csv file

--- Pokemon/PokeCell.swift
+++ Pokemon/PokeCell.swift
@@ -9,17 +9,24 @@
 import UIKit
 
 class PokeCell: UICollectionViewCell {
-    
-    @IBOutlet weak var thumbImg: UIImageView!
-    @IBOutlet weak var nameLbl: UILabel!
-    
-    var pokemon: Pokemon!
-    
-    func configureCell(pokemon: Pokemon){
-        self.pokemon = pokemon
         
-        nameLbl.text = self.pokemon.name.capitalizedString
-        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
-    }
+        @IBOutlet weak var thumbImg: UIImageView!
+        @IBOutlet weak var nameLbl: UILabel!
+        
+        var pokemon: Pokemon!
+        
+        required init?(coder aDecoder: NSCoder){
+            super.init(coder: aDecoder)
+            
+            layer.cornerRadius = 5.0
+        }
+        
+        func configureCell(pokemon: Pokemon){
+            self.pokemon = pokemon
+            
+            nameLbl.text = self.pokemon.name.capitalizedString
+            thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+        }
+        
     
 }
