8cdbf4b85a800104bcfaf44f24350918bc5f3552
Julia Waggoner <jjwaggoner27@gmail.com>
2016-01-24 20:05:33 -0600

    create custom cells

---
+++ Pokemon/PokeCell.swift
@@ -0,0 +1,25 @@
+//
+//  PokeCell.swift
+//  Pokemon
+//
+//  Created by Julia Waggoner on 1/24/16.
+//  Copyright © 2016 Julia Waggoner. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    func configureCell(pokemon: Pokemon){
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalizedString
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+    }
+    
+}
